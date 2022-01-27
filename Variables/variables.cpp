3d142cfe31876954988d179d5a2dd5f58b3dc08e
Aayam Bhatt <95232052+aayambhatt@users.noreply.github.com>
2022-01-27 19:25:48 +0530

    Create variables.cpp

---
+++ Variables/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int age = 28;
+    string CharName = "Rick";
+
+    cout <<"There was a man named " << CharName << ", he worked in cybersecurity" << endl;
+    cout <<"His age was " << age << " years old" << endl;
+
+    return 0;
+
+}
